c37eb20e2c2a7187c7baed252da5822a34b8e59f
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-29 10:28:27 +0200

    Add capital display to main list

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
-
 $('#search').click(searchCountries);
+
 function searchCountries() {
-  	var countryName = $('#country-name').val();
-  	if(!countryName.length) countryName = 'Poland';
-  	$.ajax({
-  		url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+	var countryName = $('#country-name').val();
+	if (!countryName.length) countryName = 'Poland';
+	$.ajax({
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList
+	});
 }
-var lowerCountriesList = $('#lower-list');
 
 function showCountriesList(resp) {
+	countriesList.empty();
+	resp.forEach(function(item) {
+		$('<li>').text(item.name).appendTo(countriesList);
+		$('<li>').text(item.capital).appendTo(countriesList);
+	});
+}
+/* Nie działa dodawanie do zagnieżdżonej listy nazwy stolicy 
+var lowerCountriesList = $('#lower-list');
+function showCountriesList(resp) {
     countriesList.empty();
+    lowerCountriesList.empty();
    resp.forEach(function(item){
    	$('<li>').text(item.name).appendTo(countriesList);
-   	$('<li>').text(item.capital).appendTo(countriesList);
+   	$('<li>').text(item.capital).appendTo(lowerCountriesList);
 });
 }
+*/
